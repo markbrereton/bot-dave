b1238ee5ba229a8db4521e88b41725c55d8e6a0a
Alex Tsourakis <alex.tsr@gmail.com>
2017-11-17 12:37:48 +0100

    make log level configurable

--- dave/log.py
+++ dave/log.py
@@ -4,9 +4,16 @@ A simple module setting up the Python logger for logging to stdout
 """
 
 import logging
+from os import environ
 
 logger = logging.getLogger("dave")
-logger.setLevel(logging.DEBUG)
+level = environ.get("LOG_LEVEL")
+if level.lower() == "debug":
+    logger.setLevel(logging.DEBUG)
+elif level.lower() == "info":
+    logger.setLevel(logging.INFO)
+else:
+    logger.setLevel(logging.WARN)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
