19dfb7b1f0176bcc683ebe1e50e333cc15f620c2
Alex Tsourakis <alex.tsr@gmail.com>
2017-12-01 22:30:34 +0100

    Restructure PSQL connection

--- dave/store.py
+++ dave/store.py
@@ -24,15 +24,17 @@ class Store(object):
         data = json.dumps(data)
         sql = "INSERT INTO events (event_id, data) VALUES ('{0}', $${1}$$) ON CONFLICT (event_id) DO UPDATE SET " \
               "data=$${1}$$;".format(event_id, data)
-        self.cur.execute(sql)
+        cur = self.conn.cursor()
+        cur.execute(sql)
         self.conn.commit()
 
     def retrieve_event(self, event_id):
         if not event_id:
             return {}
         sql = "SELECT data FROM events WHERE event_id='{}';".format(event_id)
-        self.cur.execute(sql)
-        resp = self.cur.fetchone()
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        resp = cur.fetchone()
         return json.dumps(resp)
 
     def retrieve_events(self, event_ids):
@@ -41,8 +43,9 @@ class Store(object):
             return resp
         event_ids = ["$${}$$".format(e) for e in event_ids]
         sql = "SELECT event_id, data FROM events WHERE event_id IN ({});".format(','.join(event_ids))
-        self.cur.execute(sql)
-        all_events = self.cur.fetchall()
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        all_events = cur.fetchall()
         for event_id, data in all_events:
             resp[event_id] = json.loads(data)
         return resp
@@ -54,7 +57,9 @@ class Store(object):
     def retrieve_all_events(self):
         resp = {}
         sql = "SELECT event_id, data FROM events;"
-        self.cur.execute(sql)
+
+        cur = self.conn.cursor()
+        cur.execute(sql)
         all_events = self.cur.fetchall()
         self.conn.commit()
         for event_id, data in all_events:
